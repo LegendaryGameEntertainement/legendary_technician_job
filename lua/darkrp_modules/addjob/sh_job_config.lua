2da7ce3016bc45ba361cca05a99dd22a1f2637cc
LegendaryGame-Entertainement <legendarygame.entertainement@gmail.com>
2026-01-23 14:38:00 +0100

    [Add] Base file

---
+++ lua/darkrp_modules/addjob/sh_job_config.lua
@@ -0,0 +1,24 @@
+DarkRP.createCategory{
+    name = "Technician",
+    categorises = "jobs",
+    startExpanded = true,
+    color = Color(255, 0, 0, 255),
+    canSee = function(ply) return true end,
+    sortOrder = 100,
+}
+
+
+TEAM_TECHNICIEN = DarkRP.createJob("Technicien", {
+    color = Color(214, 214, 214, 255),
+    model = "models/player/odessa.mdl",
+    description = [[Réparer des trucs]],
+    weapons = {""},
+    command = "lg_technicien",
+    max = 4,
+    salary = 500,
+    admin = 0,
+    vote = false,
+    category = "Technician",
+    hasLicense = false
+})
+
