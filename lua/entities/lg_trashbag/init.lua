dbec82710113ba783fd60a7506a6afdc74dbb481
LegendaryGame-Entertainement <legendarygame.entertainement@gmail.com>
2026-01-23 14:46:31 +0100

    [Add] Trash bag entitie

---
+++ lua/entities/lg_trashbag/init.lua
@@ -0,0 +1,36 @@
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+include("shared.lua")
+
+function ENT:Initialize()
+    self:SetModel("models/props_junk/garbage_bag001a.mdl")
+    
+    self:PhysicsInit(SOLID_VPHYSICS)
+    self:SetMoveType(MOVETYPE_VPHYSICS)
+    self:SetSolid(SOLID_VPHYSICS)
+    self:SetUseType(SIMPLE_USE)
+    
+    local phys = self:GetPhysicsObject()
+    if IsValid(phys) then
+        phys:Wake()
+    end
+end
+
+function ENT:Use(activator, caller)
+    if IsValid(activator) and activator:IsPlayer() then
+        -- Tu peux ajouter ici la logique pour donner le sac au joueur
+        -- Exemple : activator:Give("weapon_trash_bag")
+        self:Remove()
+    end
+end
+
+function ENT:SpawnFunction(ply, tr, ClassName)
+    if not tr.Hit then return end
+    
+    local ent = ents.Create(ClassName)
+    ent:SetPos(tr.HitPos + tr.HitNormal * 10)
+    ent:Spawn()
+    ent:Activate()
+    
+    return ent
+end
---
+++ lua/entities/lg_trashbag/shared.lua
@@ -0,0 +1,7 @@
+ENT.Type = "anim"
+ENT.Base = "base_gmodentity"
+ENT.PrintName = "Sac Poubelle"
+ENT.Author = "Ton Nom"
+ENT.Category = "SCP RP - Technician"
+ENT.Spawnable = true
+ENT.AdminOnly = false
